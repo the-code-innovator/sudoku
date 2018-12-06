5cf30f51c032e700818b5eccdd200cb398fded8d
Aravind M <the.code.innovator@gmail.com>
2018-12-06 13:42:09 +0530

    Solver::init

--- src/main/java/org/thecodeinnovator/sudoku/App.java
+++ src/main/java/org/thecodeinnovator/sudoku/App.java
@@ -4,17 +4,22 @@ import java.io.IOException;
 
 import org.thecodeinnovator.sudoku.console.InfoPrinter;
 import org.thecodeinnovator.sudoku.primitives.Grid;
+import org.thecodeinnovator.sudoku.solver.Solver;
 
 public class App {
 	public static InfoPrinter info;
 	public static Grid puzzle;
+	public static Solver solver;
     public static void main(String[] args) throws IOException {
 		info = new InfoPrinter(System.out);
 		puzzle = new Grid(args[0]);
+		solver = new Solver(puzzle.matrix);
 		info.setInformation("PATH -> " + args[0]);
 		info.putInformation();
 		puzzle.printOrder();
 		puzzle.printDimension();
 		puzzle.printGrid();
+		solver.printSolvedGrid();
+
     }
 }
--- src/main/java/org/thecodeinnovator/sudoku/solver/Solver.java
+++ src/main/java/org/thecodeinnovator/sudoku/solver/Solver.java
@@ -1,10 +1,38 @@
 package org.thecodeinnovator.sudoku.solver;
 
-import org.thecodeinnovator.sudoku.primitives.Grid;
+import java.util.ArrayList;
+
+import org.thecodeinnovator.sudoku.console.InfoPrinter;
 
 public class Solver {
-	public Character[][] solvedGrid;
-	public Grid inputGrid;
-	public Solver(Grid inputGrid) {
+	public Character[][] inputGrid;
+	public ArrayList<ArrayList<Character>> arrayGrid;
+	public InfoPrinter info;
+	public Solver(ArrayList<ArrayList<Character>> inputGrid) {
+		this.arrayGrid = inputGrid;
+		this.info = new InfoPrinter(System.out);
+		int length = inputGrid.size();
+		this.inputGrid = new Character[length][length];
+		for (int i = 0; i < length; i++) {
+			ArrayList<Character> row = inputGrid.get(i);
+			for (int j = 0; j < length; j++) {
+				Character atIJ = row.get(j);
+				this.inputGrid[i][j] = new Character(atIJ);
+			}
+		}
+	}
+	public String gridString() {
+		String grid = "";
+		for (ArrayList<Character> list : this.arrayGrid) {
+			grid = new String(grid + "\n");
+			for (Character item : list) {
+				grid = new String(grid + item + "\t");
+			}
+		}
+		return grid;
+	}
+	public void printSolvedGrid() {
+		this.info.setInformation("PUZZLE -> " + this.gridString());
+		this.info.putInformation();
 	}
 }
